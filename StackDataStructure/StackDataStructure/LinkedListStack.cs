97816a6317cbd06ee61d447361ef525efc66a279
akshata2005 <akshatasawant1805@gmail.com>
2022-01-29 19:04:19 +0530

    [Akshata]  created the linkedList

---
+++ StackDataStructure/StackDataStructure/LinkedListStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStructure
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        //customlinkedlist linked
+        public void push(int newData)
+        {
+            Node newNode = new Node(newData);
+            if(this.top != null)
+            {
+                newNode.next = this.top;
+            }
+            this.top=newNode;
+            Console.WriteLine("New node {0} is added",newNode.data);
+        }
+        //public void Display()
+        //{
+        //    Node temp=this.top
+        //}
+    }
+}
---
+++ StackDataStructure/StackDataStructure/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStructure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            CustomLinkedList linkedList = new CustomLinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            Console.WriteLine();
+            Console.WriteLine("insert at first");
+            linkedList.AddLast(70);
+            linkedList.AddLast(30);
+            linkedList.AddLast(56);
+        }
+    }
+}
